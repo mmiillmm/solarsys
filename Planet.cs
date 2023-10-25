b9eb6d731fb9b7e55831565cd0724aa69c7dfc6b
Makai Kristof <kmakai520@gmail.com>
2023-10-25 09:38:49 +0200

    Add project files.

---
+++ Planet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solsys
+{
+    internal class Planet
+    {
+        public string planetName { get; set; }
+        public int planetMoons { get; set; }
+        public float planetVolume { get; set; }
+
+        public Planet(string line)
+        {
+            var data = line.Split(";");
+            planetName = data[0];
+            planetMoons = int.Parse(data[1]);
+            planetVolume = float.Parse(data[2].Replace(".", ","));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Text;
+
+namespace solsys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Planet> planets = new List <Planet>();
+
+            var reader = new StreamReader(
+                path: "C:\\Users\\Ny19MakaiK\\source\\repos\\solsys\\bin\\Debug\\solsys.txt",
+                encoding: Encoding.UTF8);
+
+            while (!reader.EndOfStream)
+            {
+                planets.Add(new Planet(reader.ReadLine()));
+            }
+
+            Console.WriteLine($"3.1: {planets.Count} bolygó van a naprendszerben");
+
+            var avgMoon = planets.Average(a => a.planetMoons);
+            Console.WriteLine($"3.2: a naprendszerben egy bolygónak átlagosan {avgMoon} holdja van");
+
+            var maxPlanVol = planets.Single(a => a.planetVolume == planets.Max(b => b.planetVolume));
+            Console.WriteLine($"3.3: a legnagyobb térfogatú bolygó a {maxPlanVol.planetName}");
+
+            Console.Write("írd be a keresett bolygó nevét: ");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(planets.Single(a => a.planetName == input) != null ?
+                "Van ilyen bolygó a listában." :
+                "Nincs ilyen bolygó a listában");
+
+
+            Console.Write("írj be egy egész számot: ");
+
+            int inputNum = int.Parse(Console.ReadLine());
+
+            var moonCount = planets.Where(a => a.planetMoons > inputNum).Select(a => a.planetName);
+
+            Console.WriteLine($"a következő bolygóknak van {inputNum}-nál/nél több holdja: ");
+
+            foreach (var d in moonCount)
+            {
+                Console.Write($"[{d}] ");
+            }
+
+            Console.WriteLine("c:");
+
+            Console.ReadKey();
+        }
+    }
+}
